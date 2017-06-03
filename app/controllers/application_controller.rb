5c0a0ade1eccce6b7b34b88ea9a69565f8787a50
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-06-02 21:59:12 -0400

    First commit Master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :nuevo_mensaje
+
+
+  private
+
+  def nuevo_mensaje
+    @mensaje = "Esto está en un Callback"
+  end
+
+
 end
--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,7 @@
 class LandingsController < ApplicationController
+  before_action :nuevo_mensaje, only: :Z
+  #before_action :before_antes, excep: :Z#
+  #before_action :before_antes, [:Z, :Y]#
   def x
   end
 
@@ -6,5 +9,10 @@ class LandingsController < ApplicationController
   end
 
   def z
+    @val = []
+    20.times do |i|
+      @val << "nombre#{i+1}"
+    end
   end
+
 end
